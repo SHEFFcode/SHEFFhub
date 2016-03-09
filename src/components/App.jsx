0cb4da4f31e6ad6f2168b21a509f3035c4acfe94
Yuriy Shefer <sheff@sheffmachine.com>
2016-03-08 21:32:28 -0800

    set up the github API connection, create a constructor function for the App class, set state and default props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,35 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends Component {
+
+	constructor(props){
+		super(props);
+		this.state = {
+			username: 'SHEFFcode',
+			userData: [],
+			userRepos: [],
+			perPage: 5
+		}
+	}
+
+
 	render(){
 		return (
 			<div>
-				My App
+				{this.state.username}
 			</div>
 		)
 	};
 }
 
-export default App
+App.propTypes = {
+	clientId: React.PropTypes.string,
+	clientSecret: React.PropTypes.string
+};
+
+App.defaultProps = {
+	clientId: 'dd02448d317fb0fb9556',
+	clientSecret: '25b4e93ed92455f5088f8de985b360239dac741e'
+}
+
+export default App;
